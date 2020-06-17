2658932009c5a81527552f34685d78cf8a69395b
rafaelvieirab <rafaelviebar@gmail.com>
2020-06-17 17:08:52 -0300

    feat: Criando as Tabelas

--- server/src/database/migrations/02_create_point_item.ts
+++ server/src/database/migrations/02_create_point_item.ts
@@ -1,3 +1,21 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {
+    return knex.schema.createTable('point_item', table => {
+        table.increments('id').primary();
+
+        table.string('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('point');
+
+        table.string('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('item');
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_item');
+}
