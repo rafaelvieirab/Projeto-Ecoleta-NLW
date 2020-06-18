6f396d72be8c0fba11d08fb31e2242db469a2610
rafaelvieirab <rafaelviebar@gmail.com>
2020-06-18 15:51:03 -0300

    feat: Listando os items, e criando a rota para exibi-los

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json( { message: "Hello Wolrd!" } );
+routes.get('/items', async (request, response) => {
+    const items = await knex('item').select('*'); // SELECT * FROM item
+
+    const serializedItems = items.map( item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    });
+    return response.json(serializedItems);
 });
 
 export default routes;
